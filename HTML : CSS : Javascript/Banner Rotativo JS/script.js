7c228b99ce875e1dec94a8a3276aebe88db6ad18
Hugo Miguel Deus <138576768+HugoDeus@users.noreply.github.com>
2024-02-07 08:47:59 +0000

    add project

---
+++ HTML : CSS : Javascript/Banner Rotativo JS/script.js
@@ -0,0 +1,36 @@
+let fotos = ['banner1.png', 'banner2.png', 'banner3.png']
+
+function bannerswitch(ft){
+    document.querySelector('.imagem-banner').src = "imagens/" + fotos[ft]
+}
+
+let bannertroca = 0
+
+bannerswitch(bannertroca)
+
+
+let contar = setInterval(function relogio(){
+    bannertroca++
+    if(bannertroca >= fotos.length){
+        bannertroca = 0
+    }
+    bannerswitch(bannertroca)
+}, 4000)
+
+document.getElementById('btiniciar').addEventListener('click', function(){
+    contar = setInterval(function relogio(){
+        bannertroca++
+        if(bannertroca >= fotos.length){
+            bannertroca = 0
+        }
+        bannerswitch(bannertroca)
+    }, 4000)
+    document.getElementById("btiniciar").disabled = true;
+    document.getElementById("btparar").disabled = false;
+})
+
+document.getElementById("btparar").addEventListener("click", function() {
+    clearInterval(contar);
+    document.getElementById("btiniciar").disabled = false;
+    document.getElementById("btparar").disabled = true;
+});
---
+++ WEb Pages/loja plantas/js/script.js
@@ -0,0 +1,7 @@
+function enviar() {
+    let emailInput = document.getElementById("formulario").value;
+    let mensagemInput = document.getElementById("textoform").value;
+
+    
+    alert('Pedido de Contacto enviado!!');
+}
